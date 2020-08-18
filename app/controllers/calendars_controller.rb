0d806ea76bd29290c644c5ff024b2f8099d75016
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-08-19 00:03:27 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,26 +16,24 @@ class CalendarsController < ApplicationController
 
   def plan_params
     params.require(:plan).permit(:date, :plan)
-    
   end
 
   def get_week
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    require "date"
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)','(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
     @week_days = []
-
     @plans = Plan.where(date: @todays_date..@todays_date + 7)
-
+    
     7.times do |x|
+      @wday = wdays[@todays_date.wday + x]
+      
       plans = []
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date+x).month, :date => (@todays_date+x).day, :plans => plans}
+      days = { :month => (@todays_date+x).month, :date => (@todays_date+x).day, :wday => @wday, :plans => plans}
       @week_days.push(days)
     end
 
